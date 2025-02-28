feab3cf61d325c8be35504801dd9b2f80996df69
lyanbrito <lyanbrito613@gmail.com>
2025-02-28 14:40:48 -0300

    chore:fixed-logical-problem

--- src/aula5/Primo.java
+++ src/aula5/Primo.java
@@ -3,6 +3,7 @@ package aula5;
 import java.util.Scanner;
 
 public class Primo {
+
     public static void main(String[] args) {
         int numero;
         Scanner sc = new Scanner(System.in);
@@ -10,19 +11,29 @@ public class Primo {
         System.out.println("Dgiita um numero");
         numero = sc.nextInt();
 
-        if (numero % 2 == 0 || numero <= 1) {
-            System.out.println("não Primo");
-        } else if (numero == 2) {
-            System.out.println("Primo");
+        if (numero < 2) {
+            System.out.println("n primo");
         } else {
-            for (int i = 1; i <= numero / 2; i++) {
-                if (i % Math.floor(Math.sqrt(numero)) == 0) {
-                    System.out.println("Primo");
-                } else {
-                    System.out.println("não Primo");
+            boolean primo = true;
+            for (int i = 2; i <= Math.sqrt(numero); i++) {
+                if (numero % i == 0) {
+                    primo = false;
+                    break;
                 }
+            } if (primo) {
+                System.out.println("primo");
+            } else {
+                System.out.println("n primo");
             }
         }
-
+//        sem for
+//        if (numero == 2 || numero == 3 || numero == 5 || numero == 7) {
+//            System.out.println("Primo");
+//        } else if (numero % 2 == 0 || numero % 3 == 0 || numero % 5 == 0 || numero % 7 == 0) {
+//            System.out.println("não Primo");
+//        } else {
+//            System.out.println("Primo");
+//        }
     }
 }
+
