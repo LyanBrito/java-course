e49e97237895169632c0aa151a64a8b816916645
lyanbrito <lyanbrito613@gmail.com>
2025-02-28 15:03:46 -0300

    chore:refactored-code

--- src/aula5/Primo.java
+++ src/aula5/Primo.java
@@ -5,33 +5,42 @@ import java.util.Scanner;
 public class Primo {
 
     public static void main(String[] args) {
-        int numero;
+        int numero ;
         String S;
-        boolean continuar;
         Scanner sc = new Scanner(System.in);
+        boolean continuar = true;
 
         do {
-            System.out.println("Digita um numero (aperte S para sair)");
-            numero = sc.nextInt();
 
-            if (numero == 1) {
+        System.out.println("Digita um numero");
+        numero = sc.nextInt();
+
+        if (numero == 1) {
+            System.out.println("primo");
+        }
+        else if (numero < 2) {
+            System.out.println("n primo");
+        } else {
+            boolean primo = true;
+            for (int i = 2; i <= Math.sqrt(numero); i++) {
+                if (numero % i == 0) {
+                    primo = false;
+                    break;
+                }
+            } if (primo) {
                 System.out.println("primo");
-            } else if (numero < 2) {
-                System.out.println("n primo");
             } else {
-                boolean primo = true;
-                for (int i = 2; i <= Math.sqrt(numero); i++) {
-                    if (numero % i == 0) {
-                        primo = false;
-                        break;
-                    }
-                }
-                if (primo) {
-                    System.out.println("primo");
-                } else {
-                    System.out.println("n primo");
-                }
-//        sem for()
+                System.out.println("n primo");
+            }
+        }
+        System.out.println("Continuar? (S/N)");
+        S = sc.next();
+        continuar = S.equalsIgnoreCase("S");
+
+        }while (continuar);
+        sc.close();
+
+//        sem for
 //        if (numero == 2 || numero == 3 || numero == 5 || numero == 7) {
 //            System.out.println("Primo");
 //        } else if (numero % 2 == 0 || numero % 3 == 0 || numero % 5 == 0 || numero % 7 == 0) {
@@ -39,17 +48,7 @@ public class Primo {
 //        } else {
 //            System.out.println("Primo");
 //        }
-
-            }
-            System.out.println("Digite S para continuar ou N para sair");
-            S = sc.next();
-            continuar = S.equalsIgnoreCase("S");
-        }
-            while (continuar);
-            sc.close();
-
-        }
-
-}
+    }
+    }
 
 
