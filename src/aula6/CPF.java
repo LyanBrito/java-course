2f6010a581e97ec84703f880cc8fed870aeea59c
lyanbrito <lyanbrito613@gmail.com>
2025-03-26 11:11:05 -0300

    feat:refactored cpf exercism

--- src/aula6/CPF.java
+++ src/aula6/CPF.java
@@ -12,40 +12,37 @@ public class CPF {
         if (cpf.length() != 11) {
             System.out.println("digito invalido");
         } else {
-// Verificador 1
             String cpf1 = cpf.substring(0, 9), cpf2 = cpf.substring(0, 10);
-            int[] multi = {10, 9, 8, 7, 6, 5, 4, 3, 2}, multi2 = {11, 10, 9, 8, 7, 6, 5, 4, 3, 2};
-            int dig1, dig2, soma = 0, soma2 = 0;
+            int[] multi1 = {10, 9, 8, 7, 6, 5, 4, 3, 2}, multi2 = {11, 10, 9, 8, 7, 6, 5, 4, 3, 2};
+            int dig1, dig2, soma1 = 0, soma2 = 0;
             int cpfDig1 = Character.getNumericValue(cpf.charAt(9)), cpfDig2 = Character.getNumericValue(cpf.charAt(10));
 
-            for (int i = 0; i < cpf1.length(); i++) {
-                char result = cpf1.charAt(i);
-                int numericValue = Character.getNumericValue(result);
-                soma += numericValue * multi[i];
-            }
-            if (soma % 11 >= 2) {
-                dig1 = 11 - soma % 11;
-            } else {
-                dig1 = 0;
-            }
+// Verificador 1
+            dig1 = getDig(cpf1, multi1, soma1);
 // Verificador 2
+            dig2 = getDig(cpf2, multi2, soma2);
 
-            for (int i = 0; i < cpf2.length(); i++) {
-                char result = cpf2.charAt(i);
-                int numericValue = Character.getNumericValue(result);
-                soma2 += numericValue * multi2[i];
-            }
-            if (soma2 % 11 >= 2) {
-                dig2 = 11 - soma2 % 11;
-            } else {
-                dig2 = 0;
-            }
             if (dig1 == cpfDig1 && dig2 == cpfDig2) {
                 System.out.println("CPF verdadeiro");
             } else {
                 System.out.println("CPF invalido");
             }
         }
+    }
+
+    private static int getDig(String cpf, int[] multi, int soma) {
+        int dig;
+        for (int i = 0; i < cpf.length(); i++) {
+            char result = cpf.charAt(i);
+            int numericValue = Character.getNumericValue(result);
+            soma += numericValue * multi[i];
+        }
+        if (soma % 11 >= 2) {
+            dig = 11 - soma % 11;
+        } else {
+            dig = 0;
+        }
+        return dig;
     }
 }
 
