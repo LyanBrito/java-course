0b1c46a20a3c539eaa513fe8835c14e1157dbe15
lyanbrito <lyanbrito613@gmail.com>
2025-03-31 11:08:26 -0300

    feat:created-ATM-task

---
+++ src/aula7/Array.java
@@ -0,0 +1,19 @@
+package aula7;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Array {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int[] arr = new int[5];
+        int i = 0;
+
+        System.out.println("Digite 5 valores");
+        while (i < 5) {
+            arr[i] = sc.nextInt();
+            i++;
+     }
+        System.out.println("maior:" + arr[]);
+    }
+}
---
+++ src/aula7/asdfghjk.java
@@ -0,0 +1,21 @@
+package aula7;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class asdfghjk {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int[] arr = new int[5];
+        int i = 0;
+
+        System.out.println("Digite 8 valores");
+        while (i < 8) {
+            arr[i] = sc.nextInt();
+            i++;
+        }
+        System.out.println(arr.);
+
+
+    }
+}
