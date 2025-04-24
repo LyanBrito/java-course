37ea9fc71edcd7d161b69cdde76beec0a92be519
miguelzackk <mzsilvaw@gmail.com>
2025-04-24 10:19:01 -0300

    fix: wrong values

--- src/aula8/SalarioLiquido.java
+++ src/aula8/SalarioLiquido.java
@@ -7,6 +7,7 @@ public class SalarioLiquido {
         Scanner sc = new Scanner(System.in);
         double SalBr = 0;
         double SalLi = 0;
+        double Des = 0;
 
         System.out.println("Digite o salário: ");
         SalBr = sc.nextDouble();
@@ -23,20 +24,26 @@ public class SalarioLiquido {
         System.out.printf("\nSalario Bruto = R$%.2f", SalBr);
         System.out.println();
 
-        SalLi += c1.INSSCalc();
-        SalLi += c1.IR;
-        SalLi += c1.VT;
-        SalLi += c1.PS;
-        SalLi += c1.VR;
-        SalLi += c1.VA;
+        Des += c1.INSSCalc();
+        Des += c1.IR;
+        Des += c1.VT;
+        Des += c1.PS;
+        Des += c1.VR;
+        Des += c1.VA;
 
-        SalLi = SalBr - SalLi; 
+        SalLi = SalBr - Des;
         System.out.printf("\nDesconto INSS             = " + "  - R$%.2f", c1.INSSCalc());
         System.out.printf("\nDesconto Imposto de Renda = " + "  - R$%.2f", c1.IR);
         System.out.printf("\nDesconto Plano de Saude   = " + "  - R$%.2f", c1.PS);
         System.out.printf("\nDesconto Vale Transporte  = " + "  - R$%.2f", c1.VT);
         System.out.printf("\nDesconto Vale Refeição    = " + "  - R$%.2f", c1.VR);
         System.out.printf("\nDesconto Vale Alimentação = " + "  - R$%.2f", c1.VA);
+
+        double PerCent = (Des * 100 / SalBr);
+        System.out.println(" ");
+        System.out.println(String.format("Desconto total = R$%.2f", Des) + " ");
+        System.out.println(String.format("Desconto total = %.2f", PerCent) + "%");
+
 
         System.out.printf("\nSalario Liquido = R$%.2f", SalLi);
     }
