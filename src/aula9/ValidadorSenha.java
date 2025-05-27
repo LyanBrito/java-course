df9470b6d032b399e2858c63d0c2bed7531e838c
lyanbrito <lyanbrito613@gmail.com>
2025-05-27 10:56:34 -0300

    feat: cerated exercise

--- src/aula9/ValidadorSenha.java
+++ src/aula9/ValidadorSenha.java
@@ -10,53 +10,78 @@ public class ValidadorSenha {
             if (ch == '2' || ch == '3' || ch == '5' || ch == '7') {
                 valida = true;
                 break;
-            } else {
-                valida = false;
             }
         }
         return valida;
     }
-
     public static Boolean ValidaMai(String senha) {
         Boolean valida = false;
         for (int i = 0; i < senha.length(); i++) {
             char ch = senha.charAt(i);
-            if (senha.toUpperCase().charAt(i) == ch) {
+            if (Character.isUpperCase(ch)) {
                 valida = true;
-                asdwbreak;
-            } else {
-                valida = false;
+                break;
             }
         }
         return valida;
     }
-
     public static Boolean ValidaCar(String senha) {
         Boolean valida = false;
-        String[] car = {"/?:;.,><]}[{~^´`=+-_)(*&¨%$#@!|"};
+        String car = "/?:;.,><]}[{~^´`=+-_)(*&¨%$#@!|";
         for (int i = 0; i < senha.length(); i++) {
             char ch = senha.charAt(i);
-            for (String s : car) {
-                if (ch == s.charAt(i)) {
-                    valida = true;
-                    System.out.println(s.charAt(i));
-                    break;
-                } else {
-                    valida = false;
-                }
+            if (car.indexOf(ch) != -1) {
+                valida = true;
+                break;
+            }
+        }
+        return valida;
+    }
+    public static Boolean ValidaDupli(String senha) {
+        Boolean valida = false;
+
+        String vogais = "aAeEiIoOuU";
+        for (int i = 0; i < senha.length() -1; i++) {
+            char ch = senha.charAt(i);
+            char prxCh = senha.charAt(i + 1);
+            if (vogais.indexOf(ch) != -1 && ch == prxCh) {
+                valida = true;
+                break;
+            }
+            else {
+                valida = false;
             }
         }
         return valida;
     }
 
     public static void main(String[] args) {
+        Boolean valida = false;
         Scanner sc = new Scanner(System.in);
         System.out.println("Digite sua senha: ");
         String senha = sc.nextLine();
 
-        ValidaNum(senha);
-        ValidaMai(senha);
-        ValidaCar(senha);
+        if (ValidaCar(senha) && ValidaNum(senha) && ValidaMai(senha) && !ValidaDupli(senha)) {
+            valida = true;
+        }
+
+        if (!ValidaNum(senha)) {
+            System.out.println("Não tem numero primo!");
+        }
+        if (!ValidaMai(senha)) {
+            System.out.println("Não tem maiusculas");
+        }
+        if (!ValidaCar(senha)) {
+            System.out.println("Não tem caracteres especiais");
+        }
+        if (ValidaDupli(senha)) {
+            System.out.println("Tem Vogais duplicadas");
+        }
+        if (valida) {
+            System.out.println("Senha Correta");
+        } else {
+            System.out.println("Senha Incorreta");
+        }
 
     }
 }
